57d7b3dae214ff30d5ab055ad92725eea1c198cd
Franrey Anthony S. Saycon <franreysaycon@gmail.com>
2019-12-08 21:33:28 +0800

    Remove apollo from ssr

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,11 +1,7 @@
 import React from "react"
-import { ApolloProvider } from "@apollo/react-hooks"
-import client from "./src/apollo/client"
 import CurrentContextProvider from "./src/providers/current-country"
 import "typeface-poppins"
 
 export const wrapRootElement = ({ element }) => (
-  <ApolloProvider client={client}>
-    <CurrentContextProvider>{element}</CurrentContextProvider>
-  </ApolloProvider>
+  <CurrentContextProvider>{element}</CurrentContextProvider>
 )
