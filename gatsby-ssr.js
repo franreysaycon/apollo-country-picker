1d875cd3ab2da04953619c927455cceea82a7f71
Franrey Anthony S. Saycon <franreysaycon@gmail.com>
2019-12-08 21:31:22 +0800

    Add SSR definitions

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,7 +1,11 @@
-/**
- * Implement Gatsby's SSR (Server Side Rendering) APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/ssr-apis/
- */
+import React from "react"
+import { ApolloProvider } from "@apollo/react-hooks"
+import client from "./src/apollo/client"
+import CurrentContextProvider from "./src/providers/current-country"
+import "typeface-poppins"
 
-// You can delete this file if you're not using it
+export const wrapRootElement = ({ element }) => (
+  <ApolloProvider client={client}>
+    <CurrentContextProvider>{element}</CurrentContextProvider>
+  </ApolloProvider>
+)
